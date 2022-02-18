db22b56dc89ec1e5bf2540e764df2c8d82e9e73d
tokyo <tokyo@neptunemutual.com>
2022-02-18 22:47:46 +0530

    Fixed Read Storage Bug
    - Fixed bug in `getKey` function of store reader
    - Updated SDK version

--- src/config/store/definition.ts
+++ src/config/store/definition.ts
@@ -7,12 +7,12 @@ const definitions = {
   3: {
     env: process.env.NPM_ROPSTEN_STORE,
     next: process.env.NEXT_PUBLIC_ROPSTEN_STORE,
-    fallback: '0xe6c89b45Def81ce0CcAc29Ad59BF68B4eFEC41d5'
+    fallback: '0xeF038A2Fe38b4f13AC58765F8Fb94Cf1D31185bf'
   },
   42: {
     env: process.env.NPM_KOVAN_STORE,
     next: process.env.NEXT_PUBLIC_KOVAN_STORE,
-    fallback: '0xe6c89b45Def81ce0CcAc29Ad59BF68B4eFEC41d5'
+    fallback: '0x912f796d847A26A01c73a2aBc9D2Ef5E0aBb74c1'
   },
   97: {
     env: process.env.NPM_BSC_TESTNET_STORE,
