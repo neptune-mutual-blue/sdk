c3df8e6bb500aeaaf28979e67ad0799c5495ef90
tokyo <tokyo@neptunemutual.com>
2022-02-18 13:04:46 +0530

    Release v0.3.1-beta
    - Replaced XMLHttpRequest with axios
    - Updated namespace keys
    - Updated ABIs

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,7 +12,7 @@ import { IWrappedResult } from './IWrappedResult'
 import { Status } from './Status'
 import { PolicyDuration } from './PolicyDuration'
 import { IStoreCandidate } from './IStoreCandidate'
-import { XMLHttpRequestReadyState } from './XMLHttpRequestReadyState'
+
 import * as exceptions from './Exceptions'
 
 export {
@@ -30,6 +30,5 @@ export {
   Status,
   PolicyDuration,
   IStoreCandidate,
-  XMLHttpRequestReadyState,
   exceptions
 }
