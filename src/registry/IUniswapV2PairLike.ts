6d74f6ab49b5ba8ec38aa1f7ee875cc1f5149628
tokyo <tokyo@neptunemutual.com>
2022-02-20 13:40:10 +0530

    # Release v0.3.1-beta
    - Added new registry contracts `Stablecoin`, `IUniswapV2FactoryLike`, `IUniswapV2PairLike`, and `IUniswapV2RouterLike`
    - Updated type `ICoverInfo`
    - Fixed a bug in `addString` function of IPFSClient
    - Updated stores
    - Updated namespace keys
    - Updated ABIs

---
+++ src/registry/IUniswapV2PairLike.ts
@@ -0,0 +1,12 @@
+import { ethers } from 'ethers'
+import { ChainId } from '../types'
+import { abis } from '../config'
+import { contract } from '../utils'
+
+const getInstance = async (chainId: ChainId, fromAddress: string, signerOrProvider: ethers.providers.Provider | ethers.Signer | undefined): Promise<ethers.Contract> => {
+  return contract.getContract(chainId, fromAddress, abis.IUniswapV2PairLike, signerOrProvider)
+}
+
+export {
+  getInstance
+}
