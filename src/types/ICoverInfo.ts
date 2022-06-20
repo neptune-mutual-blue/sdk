629e2037a04fd42176cf7290ee64016fee0fce3e
flashburst <82953782+flashburst@users.noreply.github.com>
2022-06-21 04:10:34 +0530

    Updated types

--- src/types/ICoverInfo.ts
+++ src/types/ICoverInfo.ts
@@ -2,15 +2,22 @@ interface ICoverInfo {
   key: string
   coverName: string
   projectName?: string
+  vault: {
+    name: string
+    symbol: string
+  }
+  requiresWhitelist: boolean
+  supportsProducts: boolean
+  leverage: string
   tags: string[]
   about: string
-  blockchains: {
+  blockchains: Array<{
     chainId?: number
     name: string
-  }[]
+  }>
   rules: string
   exclusions: string
-  links: {
+  links?: {
     website: string
     documentation?: string
     telegram?: string
@@ -24,14 +31,14 @@ interface ICoverInfo {
   }
   pricingFloor: string
   pricingCeiling: string
-  reportingPeriod: string
-  cooldownPeriod: string
-  claimPeriod: string
+  reportingPeriod: number
+  cooldownPeriod: number
+  claimPeriod: number
   minReportingStake: string
   resolutionSources: string[]
   stakeWithFees: string
   reassurance: string
-  requiresWhitelist: boolean
+  reassuranceRate: string
 }
 
 export { ICoverInfo }
