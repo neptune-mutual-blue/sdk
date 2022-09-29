d918574b2547fcc3aaabdd649dc72adc1156b41a
flashburst <82953782+flashburst@users.noreply.github.com>
2022-09-29 16:25:38 +0530

    Optimized reading from IPFS

--- src/net/ipfs-client.ts
+++ src/net/ipfs-client.ts
@@ -53,17 +53,19 @@ class IPFSClient {
   }
 
   async getString (hash: string): Promise<string | undefined> {
-    const [url] = this.urls
+    const urls = this.urls
 
     // eslint-disable-next-line
-    return this.call({
-      hash,
-      method: 'POST',
-      url: `${url}/api/v0/cat?arg=${hash}`,
-      headers: {
-        Accept: 'application/json'
-      }
-    })
+    return Promise.any(urls.map(url => {
+      return this.call({
+        hash,
+        method: 'POST',
+        url: `${url}/api/v0/cat?arg=${hash}`,
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+    }))
   }
 
   async call (options: any): Promise<string | undefined> {
