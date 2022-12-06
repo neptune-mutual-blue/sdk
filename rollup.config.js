6a3551ee577351f1d98a3a3ddd3da664791b15e1
rupak-nm <rupak@neptunemutual.com>
2022-12-06 18:03:53 +0545

    Replaced microbundle with rollup for security audit fix Removed:   - "microbundle": "0.14.2" Added:   - "@rollup/plugin-json": "5.0.2"   - "@types/node": "18.11.11"   - "rollup": "3.5.1"   - "rollup-plugin-typescript2": "0.34.1"   - "typescript": "4.9.3"

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    isProduction && {
+      file: "./dist/index.cjs",
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: "./dist/index.js",
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      rollupCommonJSResolveHack: false,
+      clean: true,
+    }),
+    json(),
+  ],
+};
