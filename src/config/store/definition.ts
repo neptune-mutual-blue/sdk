9178d2e10136ac6dc28c2cf263fab484511d5dfc
binodnpm <tokyo@neptunemutual.com>
2023-04-02 18:55:43 +0400

    Release v0.5.1-beta
    - Added support for Base Goerli
    - Store: 0x52b124A19023edF0386a3D9e674FDd7e02b8A8A8
    - Protocol: 0x3B60152bFEA33B894e06291aA2bB3404b8dfdC2B

--- src/config/store/definition.ts
+++ src/config/store/definition.ts
@@ -10,6 +10,11 @@ export const getDefinition = (): any => {
       next: process.env.NEXT_PUBLIC_ARBITRUM_STORE,
       fallback: ''
     },
+    84531: {
+      env: process.env.NPM_BASE_GOERLI_STORE,
+      next: process.env.NEXT_PUBLIC_BASE_GOERLI_STORE,
+      fallback: ''
+    },
     80001: {
       env: process.env.NPM_MUMBAI_STORE,
       next: process.env.NEXT_PUBLIC_MUMBAI_STORE,
--- src/types/ChainId.ts
+++ src/types/ChainId.ts
@@ -1,6 +1,7 @@
 enum ChainId {
   Invalid = 0,
   Ethereum = 1,
+  BaseGoerli = 84531,
   Mumbai = 80001,
   Fuji = 43113,
   Arbitrum = 42161, // Arbitrum One
