408ee3da83d3a6c01d80298e0e4d72adee6b2977
flashburst <82953782+flashburst@users.noreply.github.com>
2023-06-19 17:30:33 +0530

    Fixed build issue

--- rollup.config.js
+++ rollup.config.js
@@ -2,39 +2,39 @@ import typescript from 'rollup-plugin-typescript2'
 
 import json from '@rollup/plugin-json'
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: [
     isProduction && {
-      file: "./dist/index.cjs",
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
+      file: './dist/index.cjs',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
     },
     {
-      file: "./dist/index.js",
-      format: "es",
-      exports: "named",
-      sourcemap: true,
-    },
+      file: './dist/index.js',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    }
   ],
   external: [
-    "@ethersproject/constants",
-    "@ethersproject/contracts",
-    "@ethersproject/solidity",
-    "@ethersproject/strings",
-    "axios",
-    "uuid",
-    "@ethersproject/abi",
-    "@ethersproject/keccak256",
+    '@ethersproject/constants',
+    '@ethersproject/contracts',
+    '@ethersproject/solidity',
+    '@ethersproject/strings',
+    'axios',
+    'uuid',
+    '@ethersproject/abi',
+    '@ethersproject/keccak256'
   ],
   plugins: [
     typescript({
       rollupCommonJSResolveHack: false,
-      clean: true,
+      clean: true
     }),
-    json(),
-  ],
-};
+    json()
+  ]
+}
